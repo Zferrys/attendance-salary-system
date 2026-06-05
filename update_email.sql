f77558167cba4689dc3232126aba7d8d51f76010
zferry <1978738217@qq.com>
2026-06-05 11:13:06 +0800

    chore: 删除冗余文件，清除 email.properties.example 中的真实凭证

--- update_email.sql
+++
@@ -1,5 +0,0 @@
--- 添加 email 字段到 employee 表
-ALTER TABLE employee ADD COLUMN email VARCHAR(100) DEFAULT NULL COMMENT '邮箱' AFTER position;
-
--- 给 wzl (E006) 设置邮箱
-UPDATE employee SET email = '3186649022@qq.com' WHERE emp_no = 'E006';
